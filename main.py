69534db64ac9b006d7fc1c978498c8df5e0a1925
Federico Gerardi <federico.gerardi03@gmail.com>
2021-09-13 11:59:41 +0200

    Arrowss

--- main.py
+++ main.py
@@ -32,16 +32,34 @@ async def on_message(message):
 
 @client.event
 async def on_reaction_add(reaction, user):
-    if user.id != 883501118863323216:
-        emojis = ["⬅️", "➡️"]
+    if user.id != client.user.id:
+        emojis = ["⬅️", "➡️", "🔄", "🕙"]
         if str(reaction.emoji) == emojis[0]:
-            print("INDIETRO")
-            reaction.clear()
+            await reaction.message.remove_reaction("⬅️", user)
+            await reaction.message.remove_reaction("⬅️", client.user)
+            await reaction.message.remove_reaction("➡️", client.user)
+            await reaction.message.remove_reaction("🔄", client.user)
+            await match_back(reaction.message)
+            
+
         elif str(reaction.emoji) == emojis[1]:
-            print("AVANTI")
-            reaction.remove(user)
+            await reaction.message.remove_reaction("➡️", user)
+            await reaction.message.remove_reaction("⬅️", client.user)
+            await reaction.message.remove_reaction("➡️", client.user)
+            await reaction.message.remove_reaction("🔄", client.user)
+            await match_forward(reaction.message)
+
+        elif str(reaction.emoji) == emojis[2]:
+            await reaction.message.remove_reaction("🔄", user)
+            await match_now(reaction.message)
+        
+        elif str(reaction.emoji) == emojis[3]:
+            await reaction.message.remove_reaction("🕙", user)
+            await reaction.message.remove_reaction("🕙", client.user)
+            await match_now(reaction.message)
+            
         else:
-            reaction.remove(user)
+            await reaction.remove(user)
 
 @client.command(name='live')
 async def _live(ctx, squadra):
@@ -224,7 +242,7 @@ async def _matches(ctx):
 
     matches = []
 
-    for match in matches_req:
+    for match in matches_req['data']:
         if match['round']['is_current']:
             my_round = { 'name': match['round']['name'], 'id': match['round']['round_id'] }
             matches.append(match)
@@ -243,14 +261,122 @@ async def _matches(ctx):
 
     message = await ctx.reply(embed=embedVar, mention_author=False)
 
-    emojis = ["⬅️", "➡️"]
+    emojis = ["⬅️", "➡️","🔄"]
 
     for emoji in emojis:
         await message.add_reaction(emoji)
 
 
-        
+async def match_back(message):
+    my_round = {}
 
+    params = (
+        ("season_id","2100"),
+    )
+    
+    matches_req = requests.get('https://app.sportdataapi.com/api/v1/soccer/matches', headers=FOOTBALL_API_HEADERS, params=params).json()
+
+    matches = []
+
+    for match in matches_req['data']:
+        if match['round']['is_current']:
+            my_round = { 'name': str(int(match['round']['name'])-1), 'id': match['round']['round_id'] }
+            
+    for match in matches_req['data']:        
+        if match['round']['name'] == my_round['name']:
+            matches.append(match)
+    
+    matches.sort(key=lambda match: datetime.datetime.strptime(match['match_start'], '%Y-%m-%d %H:%M:%S'))
+
+    embedVar = discord.Embed(
+        title=f'{my_round["name"]}a Giornata di Serie A TIM',
+        color=0x00197d
+    )
+
+    embedVar.set_thumbnail(url="https://www.legaseriea.it/assets/legaseriea/images/logo_main_seriea.png?v=34")
+
+    for match in matches:
+        embedVar.add_field(name=f"{(datetime.datetime.strptime(match['match_start'], '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=2)).strftime('%H:%M %d/%m/%Y') if match['status_code'] == 0 else f''':red_circle: LIVE {match['minute']}' ''' if match['status_code'] == 1 else ':clock10: Primo Tempo' if match['status_code'] == 11 else 'Partita Terminata'}", value=f"{str(discord.utils.get(client.emojis, name=match['home_team']['short_code']))} {'**'if match['stats']['home_score'] > match['stats']['away_score'] and match['status_code'] == 3 else ' '}{match['home_team']['name']} {f'''{match['stats']['home_score']} {'**'if match['stats']['home_score'] > match['stats']['away_score'] and match['status_code'] == 3 else ' '} - {'**'if match['stats']['home_score'] < match['stats']['away_score'] and match['status_code'] == 3 else ' '}{match['stats']['away_score']}''' if match['status_code'] != 0 else ' - '} {match['away_team']['name']}{'**' if match['stats']['home_score'] < match['stats']['away_score'] and match['status_code'] == 3 else ' '} {str(discord.utils.get(client.emojis, name=match['away_team']['short_code']))}", inline=False)
+
+    edit = await message.edit(embed=embedVar, mention_author=False)
+
+    emojis = ["🕙"]
+
+    for emoji in emojis:
+        await message.add_reaction(emoji)
+
+async def match_forward(message):
+    my_round = {}
+
+    params = (
+        ("season_id","2100"),
+    )
+    
+    matches_req = requests.get('https://app.sportdataapi.com/api/v1/soccer/matches', headers=FOOTBALL_API_HEADERS, params=params).json()
+
+    matches = []
+
+    for match in matches_req['data']:
+        if match['round']['is_current']:
+            my_round = { 'name': str(int(match['round']['name'])+1), 'id': match['round']['round_id'] }
+            
+    for match in matches_req['data']:        
+        if match['round']['name'] == my_round['name']:
+            matches.append(match)
+    
+    matches.sort(key=lambda match: datetime.datetime.strptime(match['match_start'], '%Y-%m-%d %H:%M:%S'))
+
+    embedVar = discord.Embed(
+        title=f'{my_round["name"]}a Giornata di Serie A TIM',
+        color=0x00197d
+    )
+
+    embedVar.set_thumbnail(url="https://www.legaseriea.it/assets/legaseriea/images/logo_main_seriea.png?v=34")
+
+    for match in matches:
+        embedVar.add_field(name=f"{(datetime.datetime.strptime(match['match_start'], '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=2)).strftime('%H:%M %d/%m/%Y') if match['status_code'] == 0 else f''':red_circle: LIVE {match['minute']}' ''' if match['status_code'] == 1 else ':clock10: Primo Tempo' if match['status_code'] == 11 else 'Partita Terminata'}", value=f"{str(discord.utils.get(client.emojis, name=match['home_team']['short_code']))} {'**'if match['stats']['home_score'] > match['stats']['away_score'] and match['status_code'] == 3 else ' '}{match['home_team']['name']} {f'''{match['stats']['home_score']} {'**'if match['stats']['home_score'] > match['stats']['away_score'] and match['status_code'] == 3 else ' '} - {'**'if match['stats']['home_score'] < match['stats']['away_score'] and match['status_code'] == 3 else ' '}{match['stats']['away_score']}''' if match['status_code'] != 0 else ' - '} {match['away_team']['name']}{'**' if match['stats']['home_score'] < match['stats']['away_score'] and match['status_code'] == 3 else ' '} {str(discord.utils.get(client.emojis, name=match['away_team']['short_code']))}", inline=False)
+
+    edit = await message.edit(embed=embedVar, mention_author=False)
+
+    emojis = ["🕙"]
+
+    for emoji in emojis:
+        await message.add_reaction(emoji)
+
+async def match_now(message):
+    my_round = {}
+
+    params = (
+        ("season_id","2100"),
+    )
+    
+    matches_req = requests.get('https://app.sportdataapi.com/api/v1/soccer/matches', headers=FOOTBALL_API_HEADERS, params=params).json()
+
+    matches = []
+
+    for match in matches_req['data']:
+        if match['round']['is_current']:
+            my_round = { 'name': str(match['round']['name']), 'id': match['round']['round_id'] }
+            matches.append(match)
+            
+    matches.sort(key=lambda match: datetime.datetime.strptime(match['match_start'], '%Y-%m-%d %H:%M:%S'))
+
+    embedVar = discord.Embed(
+        title=f'{my_round["name"]}a Giornata di Serie A TIM',
+        color=0x00197d
+    )
+
+    embedVar.set_thumbnail(url="https://www.legaseriea.it/assets/legaseriea/images/logo_main_seriea.png?v=34")
+
+    for match in matches:
+        embedVar.add_field(name=f"{(datetime.datetime.strptime(match['match_start'], '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=2)).strftime('%H:%M %d/%m/%Y') if match['status_code'] == 0 else f''':red_circle: LIVE {match['minute']}' ''' if match['status_code'] == 1 else ':clock10: Primo Tempo' if match['status_code'] == 11 else 'Partita Terminata'}", value=f"{str(discord.utils.get(client.emojis, name=match['home_team']['short_code']))} {'**'if match['stats']['home_score'] > match['stats']['away_score'] and match['status_code'] == 3 else ' '}{match['home_team']['name']} {f'''{match['stats']['home_score']} {'**'if match['stats']['home_score'] > match['stats']['away_score'] and match['status_code'] == 3 else ' '} - {'**'if match['stats']['home_score'] < match['stats']['away_score'] and match['status_code'] == 3 else ' '}{match['stats']['away_score']}''' if match['status_code'] != 0 else ' - '} {match['away_team']['name']}{'**' if match['stats']['home_score'] < match['stats']['away_score'] and match['status_code'] == 3 else ' '} {str(discord.utils.get(client.emojis, name=match['away_team']['short_code']))}", inline=False)
+
+    edit = await message.edit(embed=embedVar, mention_author=False)
+
+    emojis = ["⬅️", "➡️","🔄"]
+
+    for emoji in emojis:
+        await message.add_reaction(emoji)
 
 
 @client.command(name='invite')
